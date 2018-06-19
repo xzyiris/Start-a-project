7fbe1e575ad985a10b8c8aa977b29928b0e0cae5
huyunhe <swpujita@126.com>
2018-06-19 17:36:19 +0800

    add start& stop button

--- JS/Task1/js/task1.js
+++ JS/Task1/js/task1.js
@@ -1,90 +1,112 @@
+//generate 1~9 random number
+function random() {
+  return Math.floor(Math.random() * 9 + 1);
+}
 
-
-  function random() {
-    return Math.floor(Math.random() * 9 + 1);
+//map number to color string
+function colorMap(num) {
+  switch (num) {
+    case 1:
+      return "HotPink";
+    case 2:
+      return "red";
+    case 3:
+      return "LightCyan";
+    case 4:
+      return "LightGreen";
+    case 5:
+      return "purple";
+    case 6:
+      return "blue";
+    case 7:
+      return "MediumPurple";
+    case 8:
+      return "Tomato";
+    case 9:
+      return "YellowGreen";
+    case 10:
+      return "GreenYellow";
   }
+}
 
-  function colorMap(num) {
-    switch (num) {
-      case 1:
-        return "HotPink";
-      case 2:
-        return "red";
-      case 3:
-        return "LightCyan";
-      case 4:
-        return "LightGreen";
-      case 5:
-        return "purple";
-      case 6:
-        return "blue";
-      case 7:
-        return "MediumPurple";
-      case 8:
-        return "Tomato";
-      case 9:
-        return "YellowGreen";
-      case 10:
-        return "GreenYellow";
+//detect repeatable number in array
+function isContain(num, array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == num) {
+      return true;
     }
   }
+  return false;
+}
 
-  function isContain(num, array) {
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] == num) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  function select() {
-    var unitsId = new Array();
-    var units = new Array();
-    unitsId.push(random());
-    for (var i = 1; i < 3;) {
-      var tmp = random()
-      if (isContain(tmp, unitsId)) {
-        continue;
-      } else {
-        unitsId.push(tmp);
-        i++;
-      }
-    }
-    return unitsId;
-  }
-
-  function changecolor() {
-    var sets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var colors = new Array();
-    var unitsId = select();
-
-
-    //all div equals yellow
-    for (var i = 1; i < 10; i++) {
-      document.getElementById("unit-" + i).style.backgroundColor = "rgb(255, 166, 0)";
-    }
-
-
-    //randomly select colors
-    for (var i = 0; i < 3;) {
-      var tmp = random()
-      if (isContain(tmp, colors)) {
-        continue;
-      } else {
-        colors.push(tmp);
-        i++;
-      }
-    }
-    //randomly select div
-    for (i = 0; i < 3; i++) {
-      document.getElementById("unit-" + unitsId[i]).style.backgroundColor = colorMap(colors[i]);
+//randomly select 3 div
+function select() {
+  var unitsId = new Array();
+  var units = new Array();
+  unitsId.push(random());
+  for (var i = 1; i < 3;) {
+    var tmp = random()
+    if (isContain(tmp, unitsId)) {
+      continue;
+    } else {
+      unitsId.push(tmp);
+      i++;
     }
   }
+  return unitsId;
+}
 
-  function Timer() {
-    changecolor();
-    setTimeout("Timer()", 500);
+function changecolor() {
+  var colors = new Array();
+  var unitsId = select();
+
+
+  //all div equals yellow
+  for (var i = 1; i < 10; i++) {
+    document.getElementById("unit-" + i).style.backgroundColor = "rgb(255, 166, 0)";
   }
 
-  Timer();
+
+  //randomly select colors
+  for (var i = 0; i < 3;) {
+    var tmp = random()
+    if (isContain(tmp, colors)) {
+      continue;
+    } else {
+      colors.push(tmp);
+      i++;
+    }
+  }
+  //randomly select div
+  for (i = 0; i < 3; i++) {
+    document.getElementById("unit-" + unitsId[i]).style.backgroundColor = colorMap(colors[i]);
+  }
+}
+
+var status = 0;
+
+//set a timer
+function Timer() {
+  changecolor();
+  t = setTimeout("Timer()", 1000);
+}
+
+//start event
+function start(){
+  if(status == 0){
+    status = 1;
+    Timer();
+  }
+  else{
+    return;
+  }
+}
+//stop event
+function stop(){
+  window.clearTimeout(t);
+  status = 0;
+  for(i = 1;i<10;i++){
+    document.getElementById("unit-" + i).style.backgroundColor = "rgb(255, 166, 0)";
+  }
+}
+
