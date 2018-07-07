3accae856c4c445cfe75a7638361404afffbe664
huyunhe <swpujita@126.com>
2018-07-07 23:11:47 +0800

    update gitignore

---
+++ AngularTutorial/shop/controllers/productListController.js
@@ -0,0 +1,31 @@
+sportStore.constant("productListActiveClass","btn-primary")
+.constant("productListPageCount",3)
+.controller("productListCtrl",function ($scope,$filter,productListActiveClass,productListPageCount,cart) {
+  let selectedCategory = null;
+  $scope.selectedPage = 1;
+  $scope.pageSize = productListPageCount;
+
+  $scope.selectCategory = function (newCategory) {
+    selectedCategory = newCategory;
+    $scope.selectedPage = 1;
+  }
+
+  $scope.selectPage = function (newPage) {
+    $scope.selectedPage = newPage;
+  }
+
+  $scope.categoryFilterFn = function (product) {
+    return selectedCategory == null ||
+    product.category == selectedCategory;
+  }
+
+  $scope.getCategoryClass = function (category) {
+    return selectedCategory == category ? productListActiveClass : "";
+  }
+
+  $scope.addProductToCart = function (product) {
+    cart.addProduct(product.id,product.name,product.price);
+  }
+
+
+});
